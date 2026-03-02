76836fa034427157070bf17380384006ed493393
apc-cpu <apcherpion@uchicago.edu>
2026-03-02 00:37:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.title("Premier League Narrative Visualization")
 st.write("""This is an interactive data story exploring two seasons of Premier League football.
 By looking across points, goal difference, attacking rhythm, and home‑versus‑away performance, we can trace how each team’s 
-story has unfolded over the last two Premier League seasons.""")
+story has unfolded over the last two Premier League seasons.  I provide a very brief overview of cross season points comparison with a static chart in the story page, 
+with more graphics and greater opportunity for interactivity on the explore page.""")
