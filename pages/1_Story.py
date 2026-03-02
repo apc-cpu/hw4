415323e4bb3b87d3193acc1f1c03892e126b9bfd
apc-cpu <apcherpion@uchicago.edu>
2026-03-01 22:23:52 -0600

    Update 1_Story.py

--- pages/1_Story.py
+++ pages/1_Story.py
@@ -6,6 +6,12 @@ st.title("Premier League: Two Seasons in Review")
 
 team_matches, team_summary, homeaway_summary = load_data()
 
+
+st.write("""By looking across points, goal difference, attacking rhythm, and home‑versus‑away performance, we can trace how each team’s 
+story has unfolded over the last two Premier League seasons. I provide a brief overview of this in the story page, 
+with more graphics and greater opportunity for interactivity on the explore page.""")
+
+
 st.header("Who improved the most?")
 st.altair_chart(chart_team_points(team_summary), use_container_width=True)
 
